c98e98796109ae890675c47072e2a0ccbcbbadd3
GeoffreyXue <xuegeoff@gmail.com>
2023-12-11 12:04:33 -0600

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// A test implementation of serialization to structs in C++
+// A deserialized string is passed over, and must be parsed into a struct
+int main() {
+
+  std::cout << "Serialization Complete." << std::endl;
+  return 0;
+}
+
